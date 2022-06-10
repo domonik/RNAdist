fixed masking and loads indices

RNAdist/Attention/Datasets.py
@@ -176,15 +176,16 @@ class RNAPairDataset(Dataset):
         bppm = data["bppm"]
         pair_matrix = torch.cat((bppm, new), dim=-1)
         y = data["y"]
-        mask = 1
+        seq_len = len(self.rna_graphs[item][1])
+        pad_val = self.max_length - seq_len
         if not isinstance(y, int):
             if y.shape[0] < self.max_length:
-                pad_val = self.max_length - y.shape[0]
                 y = F.pad(y, (0, pad_val, 0, pad_val),
                                     "constant", 0)
-            mask = (y > 0).float()
-            # y = y.triu()
-        return x, pair_matrix, y, mask
+
+        mask = torch.zeros(self.max_length, self.max_length)
+        mask[:seq_len, :seq_len] = 1
+        return x, pair_matrix, y, mask, item
 
 
 if __name__ == '__main__':

RNAdist/Attention/prediction.py
@@ -29,7 +29,7 @@ def model_predict(
         )
         data_loader = DataLoader(
             dataset,
-            batch_size=1,
+            batch_size=2,
             shuffle=False,
             num_workers=1,
             pin_memory=False,
@@ -42,8 +42,8 @@ def model_predict(
         output = {}
         for idx, element in enumerate(iter(data_loader)):
             with torch.no_grad():
-                x, bppm, y, _ = element
-                pred = model(bppm)
+                x, bppm, y, mask, indices = element
+                pred = model(bppm, mask=mask)
                 pred = pred.numpy()
                 description, _ = dataset.rna_graphs[idx]
                 output[description] = pred

RNAdist/Attention/training.py
@@ -89,11 +89,11 @@ def setup(
 
 def unpack_batch(batch, device, config):
     if config["masking"]:
-        _, pair_rep, y, mask = batch
+        _, pair_rep, y, mask, _ = batch
         mask = mask.to(device)
         numel = torch.count_nonzero(mask)
     else:
-        _, pair_rep, y, _ = batch
+        _, pair_rep, y, _, _ = batch
         numel = y.numel()
         mask = None
     y = y.to(device)