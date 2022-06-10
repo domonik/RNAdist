c6b3a7db2fd7c28d02e554eca6f0320f5e93b1e5
domonik <schdruzzi@gmail.com>
2022-06-10 09:49:53 +0200

    added networkx to setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=DESCRIPTION,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    install_requires=["torch", "torchvision", "torchaudio"],
+    install_requires=["torch", "torchvision", "torchaudio", "networkx"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     scripts=[
