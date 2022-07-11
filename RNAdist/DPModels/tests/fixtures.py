f2c2482a2e82c8ee0d2249f8a6725590bb71fa16
domonik <schdruzzi@gmail.com>
2022-07-11 11:09:17 +0200

    added docs for cp implementation and changed low level function

--- RNAdist/DPModels/tests/fixtures.py
+++ RNAdist/DPModels/tests/fixtures.py
@@ -1,20 +1,11 @@
 import pytest
 import RNA
+from RNAdist.DPModels.clote import dp_matrix_available
 
 
 @pytest.fixture(scope="session")
 def rna_dp_access():
-    seq = "AATATAT"
-    md = RNA.md()
-    md.uniq_ML = 1
-    fc = RNA.fold_compound(seq, md)
-    fc.pf()
-    if hasattr(fc, 'exp_matrices') and hasattr(fc, 'exp_E_ext_stem'):
-        dp_mx = fc.exp_matrices
-        op = getattr(dp_mx, "get_Z", None)
-        if callable(op):
-            return True
-    return False
+    return dp_matrix_available()
 
 
 @pytest.fixture(scope="session")
